8aa7147e0b647532935528b4823fba67715cca07
Pinank Dagli <pdagli@grubhub.com>
2024-09-10 10:21:31 -0400

    update resume and bio

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -8,8 +8,8 @@ const Header = () => {
       <div className="portfolio__header-content">
         <h1 className="gradient__text">Hi, I am Pinank Dagli</h1>
         <p>
-          Aspiring Engineering Manager and a Senior Engineer with over a decade of experience leading software development and engineering teams. 
-          Adept at pioneering innovative solutions in web applications and automated loan platforms, utilizing a wide array of technologies including TypeScript, NextJs, and AWS services. 
+          Engineering Manager and a Senior Software Engineer with over a decade of experience leading software development and engineering teams. 
+          Adept at pioneering innovative solutions in web applications and automated loan platforms, utilizing a wide array of technologies including Javascript, TypeScript, React, Node.Js and AWS services. 
           Proven track record in mentoring and fostering cross-functional collaboration, driving efficiency, and ensuring high-quality product delivery.
         </p>
       </div>
